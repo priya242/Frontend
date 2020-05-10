a1f229ec98f376f78299bd96263cbf08d2cfd287
PRIYANKA Pandey <pandey@pdx.edu>
2020-05-10 15:15:11 -0700

    exercise-6 done

--- assign3/6-background.js
+++ assign3/6-background.js
@@ -1,1 +1,25 @@
 // Enter your code here
+var interval = document.getElementById("interval");
+var start = setInterval(bg,interval.value * 1000);
+
+function bg() {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+}
+
+function toggle() {
+  var element = document.getElementsByClassName("btn")[0];
+  if (element.value == "Stop") {
+    element.classList.remove("btn-danger");
+    element.classList.add("btn-primary");
+    element.setAttribute("value", "Start");
+    interval.disabled = false;
+    clearInterval(start);   
+  } 
+  else {
+    element.classList.remove("btn-primary");
+    element.classList.add("btn-danger");
+    element.setAttribute("value", "Stop");
+    start = setInterval(bg,interval.value * 1000)
+  }
+}
