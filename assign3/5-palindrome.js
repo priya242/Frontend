75bd15b3a584ae19845b9e8bf285e2c8938cb3e0
PRIYANKA Pandey <pandey@pdx.edu>
2020-05-10 12:50:59 -0700

    exercise-5 done

--- assign3/5-palindrome.js
+++ assign3/5-palindrome.js
@@ -1,1 +1,26 @@
 // Enter your code here
+function palindrome(){
+var numb = document.getElementById("number").value;
+var number = Array.from(numb);
+var result = document.getElementById("result").value;
+var i = 0;
+var j = number.length-1;
+var test = true;
+
+
+while(i < j){
+    if(number[i] != number[j]){
+        test = false;
+        break;
+    }
+    i++;
+    j--;
+}
+if(test == true){
+    result = "Yes";
+}
+else
+result = "No";
+document.getElementById("result").innerHTML = result;
+
+}
