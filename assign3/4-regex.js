2cffa9be1d4fe7789985cd63c834c574ef20f343
PRIYANKA Pandey <pandey@pdx.edu>
2020-05-10 12:00:54 -0700

    exercise 4 done

--- assign3/4-regex.js
+++ assign3/4-regex.js
@@ -1,1 +1,12 @@
 // Enter your code here
+function regex(){
+var input = document.getElementById("userinput").value;
+var output = input.split(new RegExp('[,;?.\n]', 'g'));
+var htmlTag = "<table class = 'table'>"
+for(var i =0; i < output.length; i++){
+    htmlTag = htmlTag + "<tr><td>" + output[i] + "</td></tr>"
+}
+htmlTag = htmlTag + "</table>"
+document.getElementById("results").innerHTML = htmlTag;
+//var result = output.map(result => result.trim());
+}
