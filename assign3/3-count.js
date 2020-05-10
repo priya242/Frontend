b08a1f1ecbd3fac9927288c59a78f599f6f35065
PRIYANKA Pandey <pandey@pdx.edu>
2020-05-10 02:00:46 -0700

    exercise-3 done

--- assign3/3-count.js
+++ assign3/3-count.js
@@ -1,1 +1,9 @@
 // Enter your code here
+function highlight(){
+const term = document.getElementById("input").value;
+const results = document.getElementById("intro");
+
+results.innerHTML = (results.innerHTML).replace(new RegExp(" " + term + " ","gi"),
+"<span> </span><span style = 'background-color:yellow;'>" + term + "</span><span> </span>")
+
+}   
