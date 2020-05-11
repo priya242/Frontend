1c3581d726eab33ed7969c9d5b25ea10d876ca66
PRIYANKA Pandey <pandey@pdx.edu>
2020-05-10 22:11:33 -0700

    added validation

--- assign3/5-palindrome.js
+++ assign3/5-palindrome.js
@@ -1,12 +1,15 @@
 // Enter your code here
 function palindrome() {
-  var numb = document.getElementById("number").value;
+  var numb = document.getElementById("number");
   var number = Array.from(numb);
   var result = document.getElementById("result").value;
   var i = 0;
   var j = number.length - 1;
   var test = true;
-
+if(numb.value == null || numb.value == ""){
+  document.getElementById("result").innerHTML = "Please fill in this field"
+  return;
+}
   while (i < j) {
     if (number[i] != number[j]) {
       test = false;
