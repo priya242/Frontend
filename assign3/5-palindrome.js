1b52fb1ed656ec90976d26f1f3a40671bcd22037
PRIYANKA Pandey <pandey@pdx.edu>
2020-05-10 21:47:36 -0700

    5-formatting done

--- assign3/5-palindrome.js
+++ assign3/5-palindrome.js
@@ -1,26 +1,22 @@
 // Enter your code here
-function palindrome(){
-var numb = document.getElementById("number").value;
-var number = Array.from(numb);
-var result = document.getElementById("result").value;
-var i = 0;
-var j = number.length-1;
-var test = true;
+function palindrome() {
+  var numb = document.getElementById("number").value;
+  var number = Array.from(numb);
+  var result = document.getElementById("result").value;
+  var i = 0;
+  var j = number.length - 1;
+  var test = true;
 
-
-while(i < j){
-    if(number[i] != number[j]){
-        test = false;
-        break;
+  while (i < j) {
+    if (number[i] != number[j]) {
+      test = false;
+      break;
     }
     i++;
     j--;
-}
-if(test == true){
+  }
+  if (test == true) {
     result = "Yes";
-}
-else
-result = "No";
-document.getElementById("result").innerHTML = result;
-
+  } else result = "No";
+  document.getElementById("result").innerHTML = result;
 }
